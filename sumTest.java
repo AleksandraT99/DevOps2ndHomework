ae6e3c7b2b3f2690e88bae550693675c3919eb50
AleksandraT99 <49170656+AleksandraT99@users.noreply.github.com>
2020-05-06 14:58:31 +0300

    Update sumTest.java

--- sumTest.java
+++ sumTest.java
@@ -1,13 +1,10 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
 
 public class sumTest {
 
-	@Test
 	public void sumTest() {
 		int a = 1;
 		int b = 2;
-		assertTrue(a + b == 3);
+		return a + b == 3;
 	}
 
 }
